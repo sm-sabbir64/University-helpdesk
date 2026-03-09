41cc6df230db619fe7009f1f51f772b6f46f1625
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-03-09 23:07:10 +0600

    added footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-gray-300 px-8 py-12 ">
+      
+        <div className="grid md:grid-cols-5 gap-8">
+          <div>
+            <h3 className="text-white font-semibold mb-2">
+              University — Assignment System
+            </h3>
+            <p className="text-sm">
+              University Assignment system is a platform that centralizes, tracks, and
+              manages requests or issues, converting them into unique
+              "Assignment s" for efficient resolution and communication
+            </p>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">University</h4>
+            <ul className="space-y-1 text-sm">
+              <li>About Us</li>
+              <li>Our Mission</li>
+              <li>Contact Administrator</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Services</h4>
+            <ul className="space-y-1 text-sm">
+              <li>Education & Services</li>
+              <li>Student Stories</li>
+              <li>Download Apps</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Information</h4>
+            <ul className="space-y-1 text-sm">
+              <li>Privacy Policy</li>
+              <li>Terms & Conditions</li>
+              <li>Join Us</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Social Links</h4>
+            <ul className="space-y-1 text-sm">
+              <li>@university — Assignment System</li>
+              <li>@university — Assignment System</li>
+              <li>@university — Assignment System</li>
+              <li>support@university.com</li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-left md:text-center text-gray-500 text-sm mt-8">
+          © 2025 University — Assignment System.
+          <br className="block md:hidden" /> All rights reserved.
+        </div>
+      
+    </footer>
+  );
+};
+
+export default Footer;
