32944bac4d3972ada955716f9e4bfece95f67755
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-03-13 23:40:28 +0600

    finished the button toggling

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,24 +1,41 @@
+import { useState } from "react";
 import Container from "./Container";
 
 const Buttons = () => {
-    return (
-        <Container>
-             <div className="flex justify-end gap-1 mb-12 my-12 mx-8">
-          <button className="bg-purple-100 text-purple-500 px-4 py-1 font-semibold text-lg rounded-l-lg cursor-pointer">
-            All
-          </button>
-          <button className="bg-purple-100 text-green-500 px-4 py-1 font-semibold text-lg cursor-pointer">
-            Pending
-          </button>
-          <button className="bg-purple-100 text-orange-500 px-4 py-1 font-semibold text-lg cursor-pointer">
-            Submited
-          </button>
-          <button className="bg-purple-100 text-red-500 px-4 py-1 font-semibold text-lg rounded-r-lg cursor-pointer">
-            Reviews
-          </button>
-        </div>
-      </Container>
-    );
+  const [toggleStatus, setToggleStatus] = useState("All");
+
+  console.log(toggleStatus);
+
+  return (
+    <Container>
+      <div className="flex justify-end gap-1 mb-12 my-12 mx-8">
+        <button
+          className={`toggle-btn rounded-l-lg ${toggleStatus == "All" && "!bg-purple-500 !text-white"}`}
+          onClick={() => setToggleStatus("All")}
+        >
+          All
+        </button>
+        <button
+          className={`toggle-btn ${toggleStatus == "Pending" && "!bg-purple-500 !text-white"}`}
+          onClick={() => setToggleStatus("Pending")}
+        >
+          Pending
+        </button>
+        <button
+          className={`toggle-btn ${toggleStatus == "Submited" && "!bg-purple-500 !text-white"}`}
+          onClick={() => setToggleStatus("Submited")}
+        >
+          Submited
+        </button>
+        <button
+          className={`toggle-btn rounded-r-lg ${toggleStatus == "Reviews" && "!bg-purple-500 !text-white"}`}
+          onClick={() => setToggleStatus("Reviews")}
+        >
+          Reviews
+        </button>
+      </div>
+    </Container>
+  );
 };
 
 export default Buttons;
