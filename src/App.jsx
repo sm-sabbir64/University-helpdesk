11e9e75092f774ea287fd2a0b2c0e5df0850fbb1
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-03-14 22:20:02 +0600

    added IssuesManagement file path

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import Container from "./components/Container";
-import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Toggle_btns from "./components/Toggle_btns";
+import IssuesManagement from "./components/IssuesManagement";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
 
-      {/* Box design*/}
-      <Container>
-        <div className="grid grid-cols-3 gap-5 my-12 mx-8">
-          <div className="rounded-md p-7 text-white h-62 bg-gray-600 flex flex-col justify-center items-center">
-            <h2 className="font-bold text-[35px]">Pending</h2>
-            <p className="font-semibold text-[26px]">0</p>
-          </div>
-          <div className="rounded-md p-7 text-white h-62 bg-purple-600 flex flex-col justify-center items-center">
-            <h2 className="font-bold text-[35px]">Submited</h2>
-            <p className="font-semibold text-[26px]">0</p>
-          </div>
-          <div className="rounded-md p-7 text-white h-62 bg-teal-600 flex flex-col justify-center items-center">
-            <h2 className="font-bold text-[35px]">Reviewed</h2>
-            <p className="font-semibold text-[26px]">0</p>
-          </div>
-        </div>
-      </Container>
+      <IssuesManagement></IssuesManagement>
 
-      {/* Toggle buttons */}
-     <Toggle_btns></Toggle_btns>
+      <Toggle_btns></Toggle_btns>
 
       <Footer></Footer>
     </div>
---
+++ src/components/IssuesManagement.jsx
@@ -0,0 +1,27 @@
+import Container from "./Container";
+
+const IssuesManagement = () => {
+  return (
+    <>
+      {/* Box design*/}
+      <Container>
+        <div className="grid grid-cols-3 gap-5 my-12 mx-8">
+          <div className="rounded-md p-7 text-white h-62 bg-gray-600 flex flex-col justify-center items-center">
+            <h2 className="font-bold text-[35px]">Pending</h2>
+            <p className="font-semibold text-[26px]">0</p>
+          </div>
+          <div className="rounded-md p-7 text-white h-62 bg-purple-600 flex flex-col justify-center items-center">
+            <h2 className="font-bold text-[35px]">Submited</h2>
+            <p className="font-semibold text-[26px]">0</p>
+          </div>
+          <div className="rounded-md p-7 text-white h-62 bg-teal-600 flex flex-col justify-center items-center">
+            <h2 className="font-bold text-[35px]">Reviewed</h2>
+            <p className="font-semibold text-[26px]">0</p>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default IssuesManagement;
