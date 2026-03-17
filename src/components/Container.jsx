6cf3c78c754935925d32dd6f75fc13dbb1823551
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-03-17 23:48:38 +0600

    Issue card design complete & json data image modify

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className='max-w-8xl mx-auto px-4'>
+        <div className='max-w-8xl mx-auto px-12'>
              {children}
         </div>
     );
--- src/components/Toggle_btns.jsx
+++ src/components/Toggle_btns.jsx
@@ -37,17 +37,33 @@ const Toggle_btns = ({ fetchPromise }) => {
         </div>
       </Container>
 
-      <div>
+     
+      <div className="grid grid-cols-3 gap-4 max-w-8xl mx-auto px-19 mb-15">
         {intialData.map((issue) => {
           return (
             <div
               key={issue.id}
-              className="shadow-md rounded-md cursor-pointer border-slate-100">
+              className="shadow-md rounded-md cursor-pointer bg-slate-100 p-4"
+            >
               <div>
-                <img src={issue.image} title={issue.name} alt={issue.name}  className="w-20 h-20 rounded-full" />
+                <img
+                  src={issue.image}
+                  title={issue.name}
+                  alt={issue.name}
+                  className="w-20 h-20 rounded-full"
+                />
                 <h2 className="font-semibold text-lg">{issue.name}</h2>
-                <p>{issue.email}</p>
               </div>
+
+              <div className="flex justify-between">
+                <h2 className="font-bold text-[24px]">{issue.subject}</h2>
+                <div>
+                  <span className={`font-semibold py-1 px-2 shadow ${issue.priority == "High"? "text-red-500 bg-red-100": issue.priority == "Medium"? "text-yellow-500 bg-yellow-100": "text-green-500 bg-green-100"}`}>{issue.priority}</span>
+                  <span>{issue.status}</span>
+                </div>
+              </div>
+
+              <p className="text-[17px]">{issue.description}</p>
             </div>
           );
         })}
